ed98359d0e0f75b5bbe35558574f581ea46e0720
VasilyMoose <96911395+VasilyMoose@users.noreply.github.com>
2023-04-19 21:08:56 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-recipient="user@domain.ru"
-
 log_dir="/path/"
 
 temp_file="/home/bash/http_requests.tmp"
@@ -13,9 +11,18 @@ last_run=$(cat $temp_file)
 now=$(date +%H:%M:%S)
 echo $now > $temp_file
 
-
 awk -v last_run="$last_run" '$3 > last_run {print $6}' $log_dir/nginx.log | sort | uniq -c | sort -nr | head -n 10 > ip.txt
 
 awk -v last_run="$last_run" '$3 > last_run {print $14}' $log_dir/nginx.log | sort | uniq -c | sort -nr | head -n 10 > code.txt
 
 cat $LOG_FILE | sed -E 's/.*"(GET|POST) ([^"]*).*/\2/' | sort | uniq -c | sort -nr | head -$NUM_URLS > url.txt
+
+to="user@domain.ru"
+subject="Список IP адресов, URL и кодов ответа веб сервера"
+subject="в прицепе файлы со список IP адресов, URL и кодов ответа веб сервера"
+
+attachment1="/root/ip.txt"
+attachment2="/root/code.txt"
+attachment3="/root/url.txt"
+
+echo "$body" | mail -s "$subject" "$to" -A "$attachment1 -A "$attachment2 -A "$attachment3"
