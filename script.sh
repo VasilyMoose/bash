fe2f259ff5deb28fc0bfbb0c1912a89ea2c1dce4
VasilyMoose <96911395+VasilyMoose@users.noreply.github.com>
2023-04-20 00:04:48 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-
+# путь к логам веб сервера
 log_dir="/path/"
-
+#  Место для указания временного файла для хранения результатов
 temp_file="/home/bash/http_requests.tmp"
-
+# создаем временный файл
 touch $temp_file
-
+# Получаем время последнего запуска скрипта
 last_run=$(cat $temp_file)
-
+# Создаем временный файл с текущим временем
 now=$(date +%H:%M:%S)
 echo $now > $temp_file
-
+# Получаем список IP, URL и Code с количеством запросов с момента последнего запуска скрипта и сохраняем в файлы.
 awk -v last_run="$last_run" '$3 > last_run {print $6}' $log_dir/nginx.log | sort | uniq -c | sort -nr | head -n 10 > ip.txt
 
 awk -v last_run="$last_run" '$3 > last_run {print $14}' $log_dir/nginx.log | sort | uniq -c | sort -nr | head -n 10 > code.txt
 
 cat $LOG_FILE | sed -E 's/.*"(GET|POST) ([^"]*).*/\2/' | sort | uniq -c | sort -nr | head -$NUM_URLS > url.txt
-
+# отправляем электронную почту на адрес, что мы укажем с приложенными файлами.
 to="user@domain.ru"
 subject="Список IP адресов, URL и кодов ответа веб сервера"
 subject="в прицепе файлы со список IP адресов, URL и кодов ответа веб сервера"
